5c7a32c6d80c32a104c4e225781999480479408e
Dmitry Panchenko <dpanchenko@users.noreply.github.com>
2021-12-16 03:13:46 +0200

    (chore): Documentation (#1)
    * (chore): project description
    
    * (chore): connect swagger
    
    * (chore): swagger documentation
    
    * (chore): update readme

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const express = require('express');
+const swaggerUI = require('swagger-ui-express');
+
 const app = express();
 const port = 8010;
 
@@ -10,12 +12,14 @@ const jsonParser = bodyParser.json();
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
+const swaggerSpecification = require('./src/swagger');
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
     buildSchemas(db);
 
     const app = require('./src/app')(db);
+    app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpecification));
 
     app.listen(port, () => console.log(`App started and listening on port ${port}`));
 });
---
+++ src/swagger.js
@@ -0,0 +1,23 @@
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Taxi',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:8010',
+        description: 'Taxi Rides API',
+      },
+    ],
+  },
+  apis: [
+    './src/app.js',
+    './src/schemas.js'
+  ],
+};
+
+module.exports = swaggerJsdoc(options);
