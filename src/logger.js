93452d3e78902d9603585fbe94c799f51996f8d5
Dmitry Panchenko <dmitriy.panchenko@gmail.com>
2021-12-16 03:44:32 +0200

    (chore): configure logger

---
+++ src/logger.js
@@ -0,0 +1,20 @@
+const expressWinston = require('express-winston');
+const winston = require('winston');
+
+const loggerOptions = {
+  transports: [
+      new winston.transports.File({
+          filename: `${__dirname}/logs/app.log`,
+      }),
+      new winston.transports.Console(),
+  ],
+  format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json(),
+  ),
+};
+
+module.exports = {
+  expressLogger: expressWinston.logger(loggerOptions),
+  winstonLogger: winston.createLogger(loggerOptions),
+};
